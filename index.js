332643f2481988dc648227226a18b1724901cdf6
legaev-stas <stanislav_legaev@epam.com>
2018-02-21 13:39:51 +0200

    auth was added: facebook-passport-token

--- index.js
+++ index.js
@@ -3,9 +3,30 @@ const websockify = require('koa-websocket');
 const messageHandler = require('./message-handler');
 const session = require('./session');
 
+// App
 const app = websockify(new Koa());
 
-app.ws.use(function(ctx, next) {
+
+
+// CORS
+const cors = require('koa-cors');
+
+app.use(cors({
+    origin: true, // should be a function that matches origin with allowed list
+    methods: ['GET', 'POST'],
+    expose: ['x-auth-token']
+}));
+
+
+
+
+const authRouter = require('./routers/auth');
+app.use(authRouter.routes());
+
+
+// WebSockets
+
+app.ws.use(function (ctx, next) {
     session.add(ctx);
 
     ctx.websocket.on('close', () => {
---
+++ routers/auth.js
@@ -0,0 +1,57 @@
+const Router = require('koa-router');
+const passport = require('koa-passport');
+const FacebookTokenStrategy = require('passport-facebook-token');
+const db = require('../db/models');
+const jwt = require('jsonwebtoken');
+
+const router = new Router({
+    prefix: '/auth'
+});
+
+passport.use(new FacebookTokenStrategy({
+        clientID: '254681075070412', // should be taken from Env Variables
+        clientSecret: 'b029c2117c101542f41df7428baee438' // should be taken from Env Variables
+    }, (accessToken, refreshToken, profile, done) => {
+        return db.user.findOrCreate({
+            where: {id: profile.id},
+            defaults: {
+                id: profile.id,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                email: profile.emails[0].value,
+                token: accessToken
+            }
+        }).spread(user => {
+                done(null, user.get({plain: true}));
+            }
+        )
+    }
+));
+
+
+const generateToken = (ctx, next) => {
+    ctx.req.token = jwt.sign({
+            id: ctx.state.user.id
+        }, 'my-secret', // TODO: secret should be passed through Env variables
+        {
+            expiresIn: 60 * 120
+        });
+    next();
+};
+
+const sendToken = ctx => {
+    ctx.res.writeHead(200, {
+        'x-auth-token': ctx.req.token
+    });
+    ctx.res.end(JSON.stringify({id: ctx.state.user.id}));
+};
+
+router.post('/facebook', passport.authenticate('facebook-token', {session: false}), (ctx, next) => {
+    if (!ctx.state.user) {
+        return ctx.res.send(401, 'User Not Authenticated');
+    }
+
+    next();
+}, generateToken, sendToken);
+
+module.exports = router;
