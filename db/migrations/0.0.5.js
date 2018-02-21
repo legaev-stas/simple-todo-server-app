2dfb36fa0645969580c4e5c13dd23684cb03df22
legaev-stas <stanislav_legaev@epam.com>
2018-02-21 11:24:11 +0200

    user model and db migration script were created

---
+++ db/migrations/0.0.5.js
@@ -0,0 +1,25 @@
+module.exports = {
+    up: function (queryInterface, Sequelize) {
+        return Promise.all([
+            queryInterface.createTable('users', {
+                id: {
+                    type: Sequelize.STRING,
+                    primaryKey: true
+                },
+                firstName: Sequelize.STRING,
+                lastName: Sequelize.STRING,
+                email: Sequelize.STRING,
+                token: Sequelize.STRING,
+                createdAt: Sequelize.DATE,
+                updatedAt: Sequelize.DATE
+            })
+        ]);
+    },
+
+    down: function (queryInterface) {
+        return Promise.all([
+            queryInterface.dropTable('users')
+        ]);
+    }
+};
+
---
+++ db/models/user.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('user', {
+        id: {
+            type: DataTypes.STRING,
+            primaryKey: true
+        },
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        token: DataTypes.STRING,
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE
+    });
+};
